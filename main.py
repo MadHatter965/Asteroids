1424181484a404df87b2c1f831fd1bc919b63146
MadHatter965 <madhatter96532@gmail.com>
2025-03-14 20:03:13 -0500

    playercharacter drawn

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # Holding on to for possible future use.
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #Start of the game and sets screen size.
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    #Starts the game loop.
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
+        # limits the frame rate to 60fps
+        dt = clock.tick(60) / 1000
     
-
 if __name__ == "__main__":
     main() 
