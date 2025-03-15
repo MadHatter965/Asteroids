5646444a6f4f6cb2242f4d2fadf3424ecb670035
MadHatter965 <madhatter96532@gmail.com>
2025-03-14 20:30:37 -0500

    implement rotation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import Player
+from player import *
 
 def main():
     # Holding on to for possible future use.
@@ -16,15 +16,20 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     #Starts the game loop.
     while True:
+        # limits the frame rate to 60fps
+        dt = clock.tick(60) / 1000
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-        # limits the frame rate to 60fps
-        dt = clock.tick(60) / 1000
+
+        
     
 if __name__ == "__main__":
     main() 
