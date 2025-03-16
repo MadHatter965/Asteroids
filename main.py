b92619477bcbe98a41061f962cdcc5323474775b
MadHatter965 <madhatter96532@gmail.com>
2025-03-15 19:13:37 -0500

    Asteroid function for destruction and splitting

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
             for shot in shots:
                 if shot.is_colliding(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
